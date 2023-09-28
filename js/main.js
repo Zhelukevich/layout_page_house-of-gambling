98ccb85ccb51590c5c8c455a199125fd7f24dd7a
Pavel Zhelukevich <pashazhelukevich1@gmail.com>
2023-09-28 20:18:23 +0300

    add burger menu

--- js/main.js
+++ js/main.js
@@ -1,1 +1,55 @@
-console.log('12412');
+document.addEventListener('DOMContentLoaded', function () {
+  // BURGER
+
+  function switchBurger() {
+    const burgerBtn = document.getElementById('burger-btn');
+    const burgerMenu = document.querySelector('.burger');
+
+    burgerBtn.addEventListener('click', () => {
+      burgerBtn.classList.toggle('burger-animation-btn');
+      burgerMenu.classList.toggle('burger-open');
+      document.body.style.overflow = 'hidden';
+
+      if (!burgerMenu.classList.contains("burger-open")) {
+        document.body.style.overflow = '';
+      }
+
+
+    })
+
+    const outsideClickListener = (event) => {
+      if (!burgerBtn.contains(event.target)) {
+        burgerBtn.classList.remove('burger-animation-btn');
+        burgerMenu.classList.remove('burger-open');
+        document.body.style.overflow = '';
+      }
+    };
+
+    document.addEventListener("click", outsideClickListener);
+  }
+
+
+
+  // language
+  function switchLanguage() {
+    const languageBtn = document.getElementById('language');
+    const languageIcon = document.getElementById('language-icon');
+    const languageSpan = languageBtn.querySelector("span");
+
+    languageBtn.addEventListener('click', () => {
+      languageBtn.classList.toggle('switch-language');
+      languageIcon.classList.toggle('switch-language-icon');
+
+
+      if (languageBtn.classList.contains("switch-language")) {
+        languageSpan.textContent = "RU";
+      } else {
+        languageSpan.textContent = "EN";
+      }
+    })
+  }
+
+  switchLanguage();
+  switchBurger();
+})
+
